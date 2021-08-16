6c8d6a1dfd9f47c7ecb35a7b7ec6f4b71f84be4c
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-16 19:00:59 +0200

    Header completed

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,3 +1,13 @@
 import React from 'react';
+import { UserIcon } from './Icons';
 
-export const Header = () => <div>header</div>;
+export const Header = () => (
+  <div>
+    <a href="./">Q & A</a>
+    <input type="text" placeholder="Search..." />
+    <a href="./signin">
+      <UserIcon />
+      <span>Sign In</span>
+    </a>
+  </div>
+);
