a98c03560f9b053d9fa8bdfbc0979f621ae86e3e
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-26 20:25:05 +0200

    Styled Header component

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { UserIcon } from './Icons';
+import './Header.css';
 
 export const Header = () => {
   const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.currentTarget.value);
   };
   return (
-    <div>
+    <div className="container">
       <a href="./">Q & A</a>
       <input
         type="text"
