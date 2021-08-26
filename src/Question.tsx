cf1c5c55f7273fc7a545fa5790e5782b3897af7b
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-26 17:44:11 +0200

    Used optional props

--- src/Question.tsx
+++ src/Question.tsx
@@ -3,11 +3,19 @@ import { QuestionData } from './QuestionsData';
 
 interface Props {
   data: QuestionData;
+  showContent?: boolean;
 }
 
-export const Question = ({ data }: Props) => (
+export const Question = ({ data, showContent }: Props) => (
   <div>
     <div>{data.title}</div>
+    {showContent && (
+      <div>
+        {data.content.length > 50
+          ? `${data.content.substring(0, 50)}...`
+          : data.content}
+      </div>
+    )}
     <div>
       {`Asked by ${data.userName} on 
         ${data.created.toLocaleDateString()}
