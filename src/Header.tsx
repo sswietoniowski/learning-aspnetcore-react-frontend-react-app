3f0b12bcf1e817b53e55f37f8b0cde7a3b25c05f
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-26 18:38:51 +0200

    Handled input change

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { UserIcon } from './Icons';
 
-export const Header = () => (
-  <div>
-    <a href="./">Q & A</a>
-    <input type="text" placeholder="Search..." />
-    <a href="./signin">
-      <UserIcon />
-      <span>Sign In</span>
-    </a>
-  </div>
-);
+export const Header = () => {
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+  };
+  return (
+    <div>
+      <a href="./">Q & A</a>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={handleSearchInputChange}
+      />
+      <a href="./signin">
+        <UserIcon />
+        <span>Sign In</span>
+      </a>
+    </div>
+  );
+};
