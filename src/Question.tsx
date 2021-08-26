a984daa54a27c5d2b88606ae24c051d922d26759
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-26 21:12:31 +0200

    Completed the home page styling

--- src/Question.tsx
+++ src/Question.tsx
@@ -1,3 +1,7 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+import { gray2, gray3 } from './Styles';
 import React from 'react';
 import { QuestionData } from './QuestionsData';
 
@@ -7,16 +11,39 @@ interface Props {
 }
 
 export const Question = ({ data, showContent = true }: Props) => (
-  <div>
-    <div>{data.title}</div>
+  <div
+    css={css`
+      padding: 10px 0px;
+    `}
+  >
+    <div
+      css={css`
+        padding: 10px 0px;
+        font-size: 19px;
+      `}
+    >
+      {data.title}
+    </div>
     {showContent && (
-      <div>
+      <div
+        css={css`
+          padding-bottom: 10px;
+          font-size: 15px;
+          color: ${gray2};
+        `}
+      >
         {data.content.length > 50
           ? `${data.content.substring(0, 50)}...`
           : data.content}
       </div>
     )}
-    <div>
+    <div
+      css={css`
+        font-size: 12px;
+        font-style: italic;
+        color: ${gray3};
+      `}
+    >
       {`Asked by ${data.userName} on 
         ${data.created.toLocaleDateString()}
         ${data.created.toLocaleTimeString()}`}
--- src/QuestionList.tsx
+++ src/QuestionList.tsx
@@ -1,3 +1,7 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+import { accent2, gray5 } from './Styles';
 import React from 'react';
 import { QuestionData } from './QuestionsData';
 import { Question } from './Question';
@@ -8,9 +12,28 @@ interface Props {
 }
 
 export const QuestionList = ({ data, renderItem }: Props) => (
-  <ul>
+  <ul
+    css={css`
+      list-style: none;
+      margin: 10px 0 0 0;
+      padding: 0px 20px;
+      background-color: #fff;
+      border-bottom-left-radius: 4px;
+      border-bottom-right-radius: 4px;
+      border-top: 3px solid ${accent2};
+      box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+    `}
+  >
     {data.map((question) => (
-      <li key={question.questionId}>
+      <li
+        key={question.questionId}
+        css={css`
+          border-top: 1px solid ${gray5};
+          :first-of-type {
+            border-top: none;
+          }
+        `}
+      >
         {renderItem ? renderItem(question) : <Question data={question} />}
       </li>
     ))}
