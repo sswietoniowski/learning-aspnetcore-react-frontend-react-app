83c7eed7a2eb5329721b9b9060b8114966929ecd
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-26 20:51:57 +0200

    Styled Header component with Emotion

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,29 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { fontFamily, fontSize, gray1, gray2, gray5 } from './Styles';
 import React from 'react';
 import { UserIcon } from './Icons';
-import styles from './Header.module.css';
 
 export const Header = () => {
   const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.currentTarget.value);
   };
   return (
-    <div className={styles.container}>
+    <div
+      css={css`
+        position: fixed;
+        box-sizing: border-box;
+        top: 0;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 10px 20px;
+        background-color: #fff;
+        border-bottom: 1px solid ${gray5};
+        box-shadow: 0 3px 7px 0 rgba(110, 112, 114, 0.21);
+      `}
+    >
       <a href="./">Q & A</a>
       <input
         type="text"
