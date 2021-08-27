a68309ac76b585e5c8c7bbbf105f4846147b3def
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-27 18:10:17 +0200

    Implemented more of the question page

--- src/QuestionPage.tsx
+++ src/QuestionPage.tsx
@@ -1,8 +1,67 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { gray3, gray6 } from './Styles';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Page } from './Page';
+import { QuestionData, getQuestion } from './QuestionsData';
 
 export const QuestionPage = () => {
+  const [question, setQuestion] = React.useState<QuestionData | null>(null);
   const { questionId } = useParams();
-  return <Page>Question Page {questionId}</Page>;
+  React.useEffect(() => {
+    const doGetQuestion = async (questionId: number) => {
+      const foundQuestion = await getQuestion(questionId);
+      setQuestion(foundQuestion);
+    };
+    if (questionId) {
+      doGetQuestion(Number(questionId));
+    }
+  }, [questionId]);
+  return (
+    <Page>
+      <div
+        css={css`
+          background-color: white;
+          padding: 15px 20px 20px 20px;
+          border-radius: 4px;
+          border: 1px solid ${gray6};
+          box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+        `}
+      >
+        <div
+          css={css`
+            font-size: 19px;
+            font-weight: bold;
+            margin: 10px 0px 5px;
+          `}
+        >
+          {question === null ? '' : question.title}
+        </div>
+        {question !== null && (
+          <React.Fragment>
+            <p
+              css={css`
+                margin-top: 0px;
+                background-color: white;
+              `}
+            >
+              {question.content}
+            </p>
+            <div
+              css={css`
+                font-size: 12px;
+                font-style: italic;
+                color: ${gray3};
+              `}
+            >
+              {`Asked by ${question.userName} on
+                ${question.created.toLocaleDateString()}
+                ${question.created.toLocaleTimeString()}`}
+            </div>
+          </React.Fragment>
+        )}
+      </div>
+    </Page>
+  );
 };
--- src/QuestionsData.ts
+++ src/QuestionsData.ts
@@ -58,6 +58,14 @@ const wait = (ms: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
+export const getQuestion = async (
+  questionId: number,
+): Promise<QuestionData | null> => {
+  await wait(500);
+  const results = questions.filter((q) => q.questionId === questionId);
+  return results.length === 0 ? null : results[0];
+};
+
 export const getUnansweredQuestions = async (): Promise<QuestionData[]> => {
   await wait(500);
   return questions.filter((q) => q.answers.length === 0);
