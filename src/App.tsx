2e0491314cddce5fc01506a36f673553abec9bfc
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-27 17:12:16 +0200

    Created component containing router

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,31 @@ import logo from './logo.svg';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
+
 function App() {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="ask" element={<AskPage />} />
+          <Route path="signin" element={<SignInPage />} />
+        </Routes>
+        <HomePage />
+      </div>
+    </BrowserRouter>
   );
 }
 
