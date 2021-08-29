dae8ad7f4cb8f139b911882fd600e7487fc6756d
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-29 12:41:48 +0200

    Implemented answer form

--- src/QuestionPage.tsx
+++ src/QuestionPage.tsx
@@ -1,11 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { gray3, gray6 } from './Styles';
+import {
+  gray3,
+  gray6,
+  Fieldset,
+  FieldContainer,
+  FieldLabel,
+  FieldTextArea,
+  FormButtonContainer,
+  PrimaryButton,
+} from './Styles';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Page } from './Page';
 import { QuestionData, getQuestion } from './QuestionsData';
 import { AnswerList } from './AnswerList';
+import { useForm } from 'react-hook-form';
 
 export const QuestionPage = () => {
   const [question, setQuestion] = React.useState<QuestionData | null>(null);
@@ -19,6 +29,7 @@ export const QuestionPage = () => {
       doGetQuestion(Number(questionId));
     }
   }, [questionId]);
+  const { register } = useForm<FormData>();
   return (
     <Page>
       <div
@@ -61,6 +72,23 @@ export const QuestionPage = () => {
                 ${question.created.toLocaleTimeString()}`}
             </div>
             <AnswerList data={question.answers} />
+            <form
+              css={css`
+                margin-top: 20px;
+              `}
+            >
+              <Fieldset>
+                <FieldContainer>
+                  <FieldLabel htmlFor="content">Your Answer</FieldLabel>
+                  <FieldTextArea id="content" name="content" {...register} />
+                </FieldContainer>
+                <FormButtonContainer>
+                  <PrimaryButton type="submit">
+                    Submit Your Answer
+                  </PrimaryButton>
+                </FormButtonContainer>
+              </Fieldset>
+            </form>
           </React.Fragment>
         )}
       </div>
