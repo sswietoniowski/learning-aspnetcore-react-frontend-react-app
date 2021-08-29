8f261f9a6af8ec910582e353548f3ed99a8f6365
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-29 12:53:25 +0200

    Implemented form validation

--- src/AskPage.tsx
+++ src/AskPage.tsx
@@ -8,6 +8,7 @@ import {
   FieldTextArea,
   FormButtonContainer,
   PrimaryButton,
+  FieldError,
 } from './Styles';
 import { useForm } from 'react-hook-form';
 
@@ -17,18 +18,51 @@ type FormData = {
 };
 
 export const AskPage = () => {
-  const { register } = useForm<FormData>();
+  const {
+    register,
+    formState: { errors },
+  } = useForm<FormData>({
+    mode: 'onBlur',
+  });
   return (
     <Page title="Ask a question">
       <form>
         <Fieldset>
           <FieldContainer>
             <FieldLabel htmlFor="title">Title</FieldLabel>
-            <FieldInput id="title" type="text" {...register('title')} />
+            <FieldInput
+              id="title"
+              type="text"
+              {...register('title', {
+                required: true,
+                minLength: 10,
+              })}
+            />
+            {errors.title && errors.title.type === 'required' && (
+              <FieldError>You must enter the question title</FieldError>
+            )}
+
+            {errors.title && errors.title.type === 'minLength' && (
+              <FieldError>The title must be at least 10 characters</FieldError>
+            )}
           </FieldContainer>
           <FieldContainer>
             <FieldLabel htmlFor="content">Content</FieldLabel>
-            <FieldTextArea id="content" {...register('content')} />
+            <FieldTextArea
+              id="content"
+              {...register('content', {
+                required: true,
+                minLength: 50,
+              })}
+            />
+            {errors.content && errors.content.type === 'required' && (
+              <FieldError>You must enter the question content</FieldError>
+            )}
+            {errors.content && errors.content.type === 'minLength' && (
+              <FieldError>
+                The content must be at least 50 characters
+              </FieldError>
+            )}
           </FieldContainer>
           <FormButtonContainer>
             <PrimaryButton type="submit">Submit Your Question</PrimaryButton>
