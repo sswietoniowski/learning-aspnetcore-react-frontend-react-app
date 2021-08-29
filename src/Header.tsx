15bfebcc89121e311f846ab56a8c63822d45bc50
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-29 19:14:40 +0200

    Implemented form submission in the search form

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import { css } from '@emotion/react';
 import { fontFamily, fontSize, gray1, gray2, gray5 } from './Styles';
 import React from 'react';
 import { UserIcon } from './Icons';
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 type FormData = {
@@ -11,12 +11,13 @@ type FormData = {
 };
 
 export const Header = () => {
-  const { register } = useForm<FormData>();
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<FormData>();
   const [searchParams] = useSearchParams();
   const criteria = searchParams.get('criteria') || '';
   const [search, setSearch] = React.useState(criteria);
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const submitForm = ({ search }: FormData) => {
+    navigate(`search?criteria=${search}`);
   };
   return (
     <div
@@ -45,7 +46,7 @@ export const Header = () => {
       >
         Q & A
       </Link>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(submitForm)}>
         <input
           {...register('search')}
           type="text"
