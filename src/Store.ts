ba69a8482effe8f39d2ba8ce851b88b67f2592bf
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-31 10:49:25 +0200

    Created actions

--- src/Store.ts
+++ src/Store.ts
@@ -17,3 +17,51 @@ const initialQuestionsState: QuestionsState = {
   viewing: null,
   searched: [],
 };
+
+export const GETTINGUNANSWEREDQUESTIONS = 'GettingUnansweredQuestions';
+
+export const gettingUnansweredQuestionsAction = () =>
+  ({
+    type: GETTINGUNANSWEREDQUESTIONS,
+  } as const);
+
+export const GOTUNANSWEREDQUESTIONS = 'GotUnansweredQuestions';
+
+export const gotUnansweredQuestionsAction = (questions: QuestionData[]) =>
+  ({
+    type: GOTUNANSWEREDQUESTIONS,
+
+    questions: questions,
+  } as const);
+
+export const GETTINGQUESTION = 'GettingQuestion';
+
+export const gettingQuestionAction = () =>
+  ({
+    type: GETTINGQUESTION,
+  } as const);
+
+export const GOTQUESTION = 'GotQuestion';
+
+export const gotQuestionAction = (question: QuestionData | null) =>
+  ({
+    type: GOTQUESTION,
+
+    question: question,
+  } as const);
+
+export const SEARCHINGQUESTIONS = 'SearchingQuestions';
+
+export const searchingQuestionsAction = () =>
+  ({
+    type: SEARCHINGQUESTIONS,
+  } as const);
+
+export const SEARCHEDQUESTIONS = 'SearchedQuestions';
+
+export const searchedQuestionsAction = (questions: QuestionData[]) =>
+  ({
+    type: SEARCHEDQUESTIONS,
+
+    questions,
+  } as const);
